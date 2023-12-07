044dc2111680dd6d7d8a6f1d16a7c38b78faccd9
mfatgg <mfatgg00@gmail.com>
2023-12-07 22:14:07 +0100

    update django to 3.0

--- wagtail_freelancer/freelancer/blocks.py
+++ wagtail_freelancer/freelancer/blocks.py
@@ -1,9 +1,4 @@
-from wagtail.admin.edit_handlers import (FieldPanel, FieldRowPanel,
-                                         InlinePanel, MultiFieldPanel,
-                                         PageChooserPanel, StreamFieldPanel)
 from wagtail.core import blocks
-from wagtail.core.fields import StreamField
-from wagtail.embeds.blocks import EmbedBlock
 from wagtail.images.blocks import ImageChooserBlock
 
 
--- wagtail_freelancer/freelancer/models.py
+++ wagtail_freelancer/freelancer/models.py
@@ -1,18 +1,11 @@
-from django.conf import settings
-from django.core.exceptions import ValidationError
 from django.db import models
-from django.template.defaultfilters import slugify
-from django.utils import six
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
-from wagtail.admin.edit_handlers import (FieldPanel, FieldRowPanel,
+from wagtail.admin.edit_handlers import (FieldPanel,
                                          InlinePanel, MultiFieldPanel,
                                          StreamFieldPanel)
 from wagtail.contrib.forms.models import AbstractForm, AbstractFormField
-from wagtail.core import blocks
 from wagtail.core.fields import RichTextField, StreamField
-from wagtail.core.models import Page, PageBase
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from freelancer.blocks import PortfolioBlock
